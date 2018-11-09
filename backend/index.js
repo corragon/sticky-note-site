f889123a8fcbe8c6027e4c53423c3719872bffcb
Corragon <corragon@gmail.com>
2018-11-08 21:40:52 -0600

    Change route endpoints

--- backend/index.js
+++ backend/index.js
@@ -30,13 +30,13 @@ app.get('/newToken', (req, res) => {
   });
 });
 
-app.get('/:token', (req, res) => {
+app.get('/tokens/:token', (req, res) => {
   console.log("Getting token with ", req.params.token);
   res.status(200);
   res.send(store.get(req.params.token));
 });
 
-app.post('/:token', (req, res) => {
+app.post('/tokens/:token', (req, res) => {
   if (!req.body.data) {
     res.status(404);
     res.send("Error: Requires data on body!");
@@ -44,6 +44,7 @@ app.post('/:token', (req, res) => {
   console.log(req.params.token, req.body.data);
   store.set(req.params.token, req.body.data);
   res.status(200);
+  res.send();
 });
 
 app.listen(3000, () => console.log('Express app listening on port 3000'));
