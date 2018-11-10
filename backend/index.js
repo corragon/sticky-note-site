0fa2f009974dbdf76ab33ad7fe0ed22363022cd9
Corragon <corragon@gmail.com>
2018-11-09 22:45:29 -0600

    Extract memory store to separate file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { generateToken } from './utils';
+import { store } from './inMemoryStore';
+
+store.set('1234', { blah: 'yay' });
 
 const app = express();
 
@@ -8,20 +11,6 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
-const store = (function () {
-  let tokens = {};
-  return {
-    get: function (token) {
-      return tokens[token] || [];
-    },
-    set: function (token, data) {
-      tokens[token] = data;
-    }
-  }
-})();
-store.set('1234', { blah: 'yay' });
-
 
 app.get('/newToken', (req, res) => {
   res.status(200);
